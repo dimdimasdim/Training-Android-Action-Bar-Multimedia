aa533110bb3ce50b6c2bb96340336d0a16a55a90
dimdimasdim <dimasprakoso@femaledaily.com>
2024-04-03 14:37:26 +0700

    feat: setup gradle for view binding and init toolbar

--- app/src/main/java/com/dimas/actionbarmultimedia/MainActivity.kt
+++ app/src/main/java/com/dimas/actionbarmultimedia/MainActivity.kt
@@ -2,10 +2,24 @@ package com.dimas.actionbarmultimedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dimas.actionbarmultimedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initializeToolbar()
+    }
+
+    private fun initializeToolbar() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.apply {
+            title = getString(R.string.app_name)
+            setDisplayHomeAsUpEnabled(false)
+        }
     }
 }
