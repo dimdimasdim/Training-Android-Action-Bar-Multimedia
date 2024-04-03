07e79611596a982b029fb23ed602c9d90f1b34bf
dimdimasdim <dimasprakoso@femaledaily.com>
2024-04-03 14:52:25 +0700

    feat: add menu in action bar

--- app/src/main/java/com/dimas/actionbarmultimedia/MainActivity.kt
+++ app/src/main/java/com/dimas/actionbarmultimedia/MainActivity.kt
@@ -2,6 +2,9 @@ package com.dimas.actionbarmultimedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.dimas.actionbarmultimedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +16,30 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initializeToolbar()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.action_profile -> {
+                Toast.makeText(this, "1", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_search -> {
+                Toast.makeText(this, "2", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_history -> {
+                Toast.makeText(this, "3", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
     }
 
     private fun initializeToolbar() {
